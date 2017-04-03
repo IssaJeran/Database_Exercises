d72583bcf27fbdd7b6a7734da25bee2697769afd
IssaJeran <jeransmith22@gmail.com>
2017-04-03 16:19:52 -0500

    Update albums_migration.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE 'albums' (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist TEXT DEFAULT 'NONE',
     name TEXT NOT NULL,
-    release_date INT DATE UNSIGNED NOT NULL,
+    release_date YEAR UNSIGNED NOT NULL,
     sales FLOAT UNSIGNED NOT NULL,
     genre TEXT NOT NULL,
     PRIMARY KEY (id)
